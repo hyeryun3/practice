6f7ee0c61ee64e298b0bfe9b89447a7e53415967
hyeryun3 <hryun3@gmail.com>
2022-11-03 16:51:11 +0900

    node-페이징 처리

--- app.js
+++ app.js
@@ -8,31 +8,50 @@ var app = express()
 app.locals.pretty = true
 app.set('view engine','pug')
 app.set('views','./views')
+app.use(express.static('public'))
 app.use(bodyParser.urlencoded({extended: false}))
 // app.use(express.static('public')) // /public 입력할 필요없이 폴더에 접근 가능
 
 app.get('/',function(req,res){
+
     
-    conn.connect()
+    var pageSize = req.query.pageSize
+    var pageNo = req.query.pageNo
+    if(pageSize == undefined || typeof pageSize == 'undefined' || pageSize == null){
+        pageSize=10
+    }else{
+        pageSize=parseInt(pageSize)
+    }
+
+    if(pageNo == undefined || typeof pageNo == 'undefined' || pageNo == null){
+        pageNo = 1
+    }else{
+        pageNo = parseInt(pageNo)
+    }
+
+    var startNo = (pageNo-1)*pageSize == 0 ? 1 : (pageNo-1)*pageSize+1
+    
     conn.query('select * from book',function(err,results,files){
         if(err){
             console.log(err)
         }
         console.log(results)
-            res.render('index', {book:results}) 
+        
+        var totalCount = results.length
+        var endNo = (pageNo * pageSize)-1 < totalCount ? (pageNo * pageSize) : totalCount
+        var endPage = (totalCount % pageSize != 0 )? totalCount / pageSize + 1 : totalCount / pageSize
+            
+        res.render('index', {book:results,startNo:startNo,endNo:endNo,endPage:Math.floor(endPage),pageSize:pageSize}) 
+            
     })
-
-    conn.end()
-    
-
 })
+
 app.get('/one',function(req,res){
     res.send('one  입니다.')
 })
 app.post('/formOk',function(req,res){
     var title = req.body.title
     var contents = req.body.contents
-    conn.connect()
 
     conn.query("insert into book(title,contents) values('"+title+"','"+ contents + "')",function(err,results,files){
         if(err){
@@ -42,7 +61,6 @@ app.post('/formOk',function(req,res){
         res.redirect('/')
         // res.render('index', {book:results}) 
     })
-    conn.end()
 
 })
 app.get('/two',function(req,res){
